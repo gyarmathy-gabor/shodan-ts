138359e2216995e7ca403a363ca7a918f98b0c82
Gyarmathy Gábor <gy.gabor03@gmail.com>
2026-03-02 15:52:07 +0100

    Add documentation to accountMethods.ts

--- src/modules/core/accountMethods.ts
+++ src/modules/core/accountMethods.ts
@@ -1,6 +1,9 @@
 import { request } from '../../utils/fetcher';
 
 export const buildAccountMethods = (baseUrl: string, apiKey: string) => ({
+  /**
+   * Returns information about the Shodan account linked to this API key.
+   */
   getProfile: async () => {
     return await request(baseUrl, 'account/profile', apiKey);
   },
